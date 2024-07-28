bba003a244ad858dd7aef633978b3a14ac521d2f
devlpmasa <devlp.masa@gmail.com>
2024-07-28 11:10:34 +0900

    Chapter8.3++

--- diary/views.py
+++ diary/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
 from .forms import InquiryForm
+from django.contrib import messages
 
 logger = logging.getLogger(__name__)
 
@@ -19,5 +20,16 @@ class InquiryView(generic.FormView):
 
     def form_valid(self, form):
         form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('dairy:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
